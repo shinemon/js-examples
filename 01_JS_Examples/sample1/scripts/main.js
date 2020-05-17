0a9f0f5efaa8a163d93129395db46cfa4d57b425
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-17 23:22:46 +0100

    My JS event example

--- 01_JS_Examples/sample1/scripts/main.js
+++ 01_JS_Examples/sample1/scripts/main.js
@@ -1,2 +1,7 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+//Event Example
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
