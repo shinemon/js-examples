325156fd3c7d648712eaf7eca9a5706427595031
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-18 23:13:45 +0100

    My JS data type example

---
+++ 01_JS_Examples/dataTypes/scripts/main.js
@@ -0,0 +1,8 @@
+
+let stringSingleQuote = 'Hello';
+let stringDoubleQuote = "Hey! Hello!";
+let stringBackTicks = `Sum of 1 and 2 is ${1 +2}`
+
+alert(stringSingleQuote);
+alert(stringDoubleQuote);
+alert(stringBackTicks);
