cc79f1f44f624920663ce5c7f6b0ed11009e78e1
mailshine <mailshine@gmail.com>
2020-05-18 23:34:41 +0100

    My JS alert prompt confirm example

--- 01_JS_Examples/04_dataTypes/scripts/main.js
+++ 01_JS_Examples/04_dataTypes/scripts/main.js
@@ -6,3 +6,6 @@ let stringBackTicks = `Sum of 1 and 2 is ${1 +2}`
 alert(stringSingleQuote);
 alert(stringDoubleQuote);
 alert(stringBackTicks);
+
+let isGreater = 4 > 3;
+alert(`4 > 3 ? ${isGreater}`)
