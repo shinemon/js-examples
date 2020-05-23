10d8024ce69e58b59d6c8ddad11d8fe1d7b50fd1
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-23 21:49:26 +0100

    prime numbers refactor

--- 01_JS_Examples/08_loops/scripts/prime.js
+++ 01_JS_Examples/08_loops/scripts/prime.js
@@ -3,22 +3,21 @@ let number = prompt("Provide Prime Number till:", 0);
 let prime= [2];
 
 if (number <= 1){
-alert("Your input should be more than 1.")
+    alert("Your input should be more than 1.")
 }
 
 if (number > 1) {
-let remainder = 0;
-for(let i = 2; i <= number; i++){
-for (let j = 2; j < i; j++){
- remainder = i % j;
+    let remainder = 0;
+    for(let i = 2; i <= number; i++){
+        for (let j = 2; j < i; j++){
+            remainder = i % j;
 
-  if (remainder == 0){
-      break;
-  }
- prime.push(i);
- break;
-}
-
-}
-alert(prime)
+            if (remainder == 0){
+            break;
+            }
+            prime.push(i);
+            break;
+            }
+    }
+    alert(prime)
 }
