e3cc603238a6d373908d6797989d48b9706c23c1
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-25 19:03:23 +0100

    js functions

--- 01_JS_Examples/09_functions/scripts/main.js
+++ 01_JS_Examples/09_functions/scripts/main.js
@@ -4,6 +4,22 @@ let name = prompt("What is your name?");
 showMessage();
 
 function showMessage(){
-alert("Hello " + name);
+    alert("Hello " + name);
 }
 
+
+
+function showMessage2(from, text){
+    alert("showMessage2 alert " + from + " : " + text);
+}
+
+showMessage2("John", "Hello");
+showMessage2("Mark")
+
+
+function showMessage3(from, text ="default value"){
+    alert("showMessage3 alert " + from + " : " + text);
+}
+
+showMessage3("John", "Hello");
+showMessage3("Mark")
