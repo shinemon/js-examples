6142610ca4adce6ac6cfbc4c1d32a34a21dfd685
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-07-19 23:19:26 +0100

    storage example

--- 02_JS_Examples/04_storage/scripts/storage.js
+++ 02_JS_Examples/04_storage/scripts/storage.js
@@ -1,10 +1,18 @@
 
 let key = 'key_1';
-
 sessionStorage.setItem(key,"session_Value_1");
-
 localStorage.setItem(key,"local_Value_1");
-
 alert("Session Storage " + sessionStorage.getItem(key));
-
 alert("Local Storage " + localStorage.getItem(key));
+
+//Set Object:
+let myObj = { studentName: 'John Smith', course: 'CSC' };
+let key2 = 'key_obj_1';
+localStorage.setItem(key2, JSON.stringify(myObj));
+alert(localStorage.getItem(key2))
+// Read item:
+let item = JSON.parse(localStorage.getItem(key2));
+
+if(window.sessionStorage){
+alert('browser support storage');
+}
