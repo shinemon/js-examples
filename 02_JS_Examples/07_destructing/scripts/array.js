4b64340d9ab9b8a83f1420864dfe83c9169f63e0
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-07-26 17:29:45 +0100

    object destructing example

--- 02_JS_Examples/07_destructing/scripts/array.js
+++ 02_JS_Examples/07_destructing/scripts/array.js
@@ -7,3 +7,29 @@ let [firstName, surname] = arr;
 
 alert(firstName);
 alert(surname);
+
+
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+this.topSalary(salaries);
+
+function topSalary(salaries){
+
+let max = 0;
+let maxName = null;
+
+for (const[name,salary] of Object.entries(salaries)){
+ if (max < salary){
+ max = salary;
+ maxName = name;
+ }
+}
+alert(maxName + ' from function.')
+ return maxName;
+
+}
