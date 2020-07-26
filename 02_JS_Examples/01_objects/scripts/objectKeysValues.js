7dea9b255deb1604bc9da4a521adf2db1233ab66
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-07-26 14:34:48 +0100

    array destructing example

--- 02_JS_Examples/01_objects/scripts/objectKeysValues.js
+++ 02_JS_Examples/01_objects/scripts/objectKeysValues.js
@@ -9,11 +9,9 @@ alert(Object.entries(salaries));
 
 function sumSalaries(salaries){
     let sum = 0;
-
     for (let salary of Object.values(salaries) ){
     sum += salary;
     }
-
     return sum;
 }
 
---
+++ 02_JS_Examples/07_destructing/scripts/array.js
@@ -0,0 +1,9 @@
+let arr = ["abc", "xyz"]
+
+// destructuring assignment
+// sets firstName = arr[0]
+// and surname = arr[1]
+let [firstName, surname] = arr;
+
+alert(firstName);
+alert(surname);
