124288a5e29c1d90de2c525e9252ff2aae4ed82f
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-01 23:37:23 +0100

    recursive simple

--- 03_JS_Examples/01_recursive/scripts/recursive.js
+++ 03_JS_Examples/01_recursive/scripts/recursive.js
@@ -4,3 +4,37 @@ function sumTo(n) {
 }
 
 alert( sumTo(100) );
+/*
+function fibonacci(n){
+ if(n <=1) return n;
+ return fibonacci(n -1) + fibonacci(n -2);
+}
+
+alert( fibonacci(5) );*/
+
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printList(list) {
+
+  alert(list.value);
+
+  if (list.next) {
+    printList(list.next);
+  }
+
+}
+
+printList(list);
