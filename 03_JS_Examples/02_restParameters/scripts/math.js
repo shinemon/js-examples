ce82c344fbee7a6c7b9b38413b12062371534d43
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-24 15:48:04 -0400

    spread parameters example

--- 03_JS_Examples/02_restParameters/scripts/math.js
+++ 03_JS_Examples/02_restParameters/scripts/math.js
@@ -1,3 +1,8 @@
 let arr = [3, 5, 1, 100, 200, 999, 1020, 3];
 
 alert( Math.max(...arr) ); // 5 (spread turns array into a list of arguments)
+
+let arr1 = [1, -2, 3, 4];
+let arr2 = [8, 3, -8, 1];
+
+alert( Math.max(...arr1, ...arr2) );
