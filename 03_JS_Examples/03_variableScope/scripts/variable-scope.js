cef3c40c780f775b9e8ce5c0ee6ce10ca3c524be
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-24 16:33:05 -0400

    variable scope examples

--- 03_JS_Examples/03_variableScope/scripts/variable-scope.js
+++ 03_JS_Examples/03_variableScope/scripts/variable-scope.js
@@ -7,14 +7,40 @@
 
 //alert(message)
 
+///////////////
 
 function sayHiBye(firstName, lastName){
     function getFullName(){
-    return firstName + " " +  lastName;
+        return firstName + " " +  lastName;
     }
-
 alert("Hello " + getFullName());
 alert("Bye! " + getFullName());
 }
-
 sayHiBye("Sam", "Son");
+
+////////////////
+
+let name = "John";
+function sayHi() {
+  alert("Hi, " + name);
+}
+name = "Pete";
+sayHi();
+
+/////////////
+function makeCounter() {
+  let count = 0;
+
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+let counter2 = makeCounter();
+
+alert( counter() ); // 0
+alert( counter() ); // 1
+
+alert( counter2() ); // ?
+alert( counter2() ); // ?
