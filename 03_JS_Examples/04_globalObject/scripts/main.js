d9f6554f18b7b24e70d985933c176c9284058e3d
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-25 23:52:18 -0400

    global variable example

--- 03_JS_Examples/04_globalObject/scripts/main.js
+++ 03_JS_Examples/04_globalObject/scripts/main.js
@@ -14,3 +14,13 @@ if (!window.Promise) {
   alert("Your browser is really old!");
 } else {
 alert("you are good to go!")}
+
+
+
+//// NEW Function
+
+//These three declarations mean the same:
+
+new Function('a', 'b', 'return a + b'); // basic syntax
+new Function('a,b', 'return a + b'); // comma-separated
+new Function('a , b', 'return a + b'); // comma-separated with spaces
