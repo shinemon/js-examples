a865b0793cccfdb3ba95190ce9dae0a4ebf886f2
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-25 22:59:49 -0400

    global variable example

---
+++ 03_JS_Examples/04_globalObject/scripts/main.js
@@ -0,0 +1,11 @@
+// make current user information global, to let all scripts access it
+window.currentUser = {
+  name: "John"
+};
+
+// somewhere else in code
+alert(currentUser.name);  // John
+
+// or, if we have a local variable with the name "currentUser"
+// get it from window explicitly (safe!)
+alert(window.currentUser.name);
