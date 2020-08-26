0beb79078f307b3d511d6bfab2665ec38b4b406f
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-25 23:26:30 -0400

    global variable example

--- 03_JS_Examples/04_globalObject/scripts/main.js
+++ 03_JS_Examples/04_globalObject/scripts/main.js
@@ -9,3 +9,8 @@ alert(currentUser.name);  // John
 // or, if we have a local variable with the name "currentUser"
 // get it from window explicitly (safe!)
 alert(window.currentUser.name);
+
+if (!window.Promise) {
+  alert("Your browser is really old!");
+} else {
+alert("you are good to go!")}
