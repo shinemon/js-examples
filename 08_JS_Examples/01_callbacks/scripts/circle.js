a675456d7bce0bee9ce589f10ea60563d77dbd34
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-27 17:12:49 -0400

    call back additional

--- 08_JS_Examples/01_callbacks/scripts/circle.js
+++ 08_JS_Examples/01_callbacks/scripts/circle.js
@@ -1,6 +1,7 @@
 
   function go() {
-    showCircle(150, 150, 100, div => {
+  ///showCircleWithoutCallBack(150,150,200);
+    showCircle(150, 150, 200, div => {
       div.classList.add('message-ball');
       div.append("Hello, world!");
     });
@@ -25,3 +26,25 @@
       });
     }, 0);
   }
+
+    function showCircleWithoutCallBack(cx, cy, radius) {
+      let div = document.createElement('div');
+      div.style.width = 0;
+      div.style.height = 0;
+      div.style.left = cx + 'px';
+      div.style.top = cy + 'px';
+      div.className = 'circle';
+      document.body.append(div);
+
+
+      setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+
+        div.addEventListener('transitionend', function handler() {
+          div.removeEventListener('transitionend', handler);
+        });
+      }, 0);
+      div.classList.add('message-ball');
+            div.append("Hello, world!");
+    }
